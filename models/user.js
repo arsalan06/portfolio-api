e3767cf1aaf46a0a621a570180d8bd9425ec6f25
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-07-11 21:45:54 +0500

    set user model

---
+++ models/user.js
@@ -0,0 +1,67 @@
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcrypt");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      fullName: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "name is required" },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "email is required" },
+          isEmail: true,
+        },
+      },
+      phoneNumber: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: { msg: "phoneNumber is required" },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [8],
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+    }
+  );
+  User.beforeCreate((user) => {
+    return hashPassword(user);
+  });
+
+  User.beforeUpdate((user) => {
+    if (user.changed("password")) {
+      return hashPassword(user);
+    }
+  });
+  User.prototype.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  };
+  return User;
+};
+
+async function hashPassword(user) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+  user.password = hashedPassword;
+}
